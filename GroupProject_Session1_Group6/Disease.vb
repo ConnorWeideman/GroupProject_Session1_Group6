814a32edaf445d54bef7cea818234f7d8a6fe07b
Josh Gerber <joshgerber22@gmail.com>
2020-10-07 16:01:49 +0200

    Created the disease class

---
+++ GroupProject_Session1_Group6/Disease.vb
@@ -0,0 +1,15 @@
+﻿' *****************************************************************
+' Team Number: 6
+' Team Member 1 Details: Gerber, Josh 220052458
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' Class name: Disease
+' *****************************************************************
+Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class Disease
+
+End Class '</Disease>
