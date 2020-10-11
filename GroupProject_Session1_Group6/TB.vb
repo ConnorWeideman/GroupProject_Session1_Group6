427498dfee21f1e82fa32ebb96ffb7a13dbdef4e
MICHAEL CAMERON RAMOS KARNER <217028123@student.uj.ac.za>
2020-10-11 17:01:42 +0200

    TB 2.2

--- GroupProject_Session1_Group6/TB.vb
+++ GroupProject_Session1_Group6/TB.vb
@@ -1,7 +1,7 @@
 ﻿' *****************************************************************
 ' Team Number: 6
 ' Team Member 1 Details: Gerber, Josh 220052458
-' Team Member 2 Details: Karner, Michael 217028123
+' Team Member 2 Details: Karner, M 217028123
 ' Team Member 3 Details: Surname, Initials (Student #)
 ' Team Member 4 Details: e.g. Smith, J (202000001)
 ' Practical: Team Project
