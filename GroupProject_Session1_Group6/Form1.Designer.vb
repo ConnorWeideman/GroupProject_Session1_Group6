e63675b1420c4fbe7011a58138a9712981173f7b
CONNOR WEIDEMAN <220145487@student.uj.ac.za>
2020-10-11 15:55:48 +0200

    Basic Form Input Design

--- GroupProject_Session1_Group6/Form1.Designer.vb
+++ GroupProject_Session1_Group6/Form1.Designer.vb
@@ -22,10 +22,63 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.txtDisplay = New System.Windows.Forms.RichTextBox()
+        Me.txtWorst = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(143, 12)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(141, 68)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Location = New System.Drawing.Point(12, 86)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(412, 542)
+        Me.txtDisplay.TabIndex = 1
+        Me.txtDisplay.Text = ""
+        '
+        'txtWorst
+        '
+        Me.txtWorst.Location = New System.Drawing.Point(12, 692)
+        Me.txtWorst.Name = "txtWorst"
+        Me.txtWorst.Size = New System.Drawing.Size(412, 22)
+        Me.txtWorst.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(182, 652)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Worst Disease:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(436, 726)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtWorst)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnStart As Button
+    Friend WithEvents txtDisplay As RichTextBox
+    Friend WithEvents txtWorst As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- GroupProject_Session1_Group6/Form1.vb
+++ GroupProject_Session1_Group6/Form1.vb
@@ -1,3 +1,169 @@
-﻿Public Class Form1
-    'Hello
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+
+' *****************************************************************
+' Team Number: assigned to team 6
+' Team Member 1 Details: Weideman, C (220145487)
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' Class name: Form1
+' *****************************************************************
+
+Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Public Class Form1
+
+    Private Diseases(2) As Disease
+    Private numWeeks As Integer
+    Private Const NL As String = vbNewLine
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        numWeeks = CInt(InputBox("How many weeks would you like to track the diseases for?"))
+
+        'Setup Diseases
+        Diseases(0) = New HIV
+        Diseases(1) = New Malaria
+        Diseases(2) = New TB
+
+        Diseases(0).Infected = CInt(InputBox("How many are initially infected with HIV at the start of tracking?"))
+        Diseases(1).Infected = CInt(InputBox("How many are initially infected with Malaria at the start of tracking?"))
+        Diseases(2).Infected = CInt(InputBox("How many are initially infected with TB at the start of tracking?"))
+
+        'Each Week Input
+        For i As Integer = 1 To numWeeks
+
+            'Input infected, deaths and recovered for each disease
+            For i1 As Integer = 0 To 2
+
+                Dim deaths, newInfected, recovered As Integer
+                Dim disease As String
+
+                If (i1 = 0) Then
+                    disease = "HIV"
+                ElseIf (i1 = 1) Then
+                    disease = "Malaria"
+                ElseIf (i1 = 2) Then
+                    disease = "TB"
+                End If
+
+                newInfected = CInt(InputBox("In week " + CStr(i) + " how many people were infected with " + disease + "?"))
+                recovered = CInt(InputBox("In week " + CStr(i) + " how many people recovered from " + disease + "?"))
+                deaths = CInt(InputBox("In week " + CStr(i) + " how many people died from " + disease + "?"))
+
+                Diseases(i1).Infected += newInfected
+                Diseases(i1).Recovered += recovered
+                Diseases(i1).Deaths += deaths
+
+            Next
+
+            'Disease Specific
+            Dim tempHIV As HIV
+            Dim tempMalaria As Malaria
+            Dim tempTB As TB
+
+            'HIV
+            tempHIV = TryCast(Diseases(0), HIV)
+            If (tempHIV) Then
+
+                Dim haveAids, blood, sexual As Integer
+
+                haveAids = CInt(InputBox("In week " + CStr(i) + " how many new people were diagnosed with AIDS?"))
+                blood = CInt(InputBox("Of those infected with HIV in week " + CStr(i) + ", how many people were infected through blood?"))
+                sexual = CInt(InputBox("Of those infected with HIV in week " + CStr(i) + " how many people were infected through sexual contact?"))
+
+                tempHIV.haveAids += haveAids
+                tempHIV.Blood += blood
+                tempHIV.Sexual += sexual
+
+                Diseases(0) = tempHIV
+
+            End If
+
+            'Malaria
+            tempMalaria = TryCast(Diseases(1), Malaria)
+            If (tempMalaria) Then
+
+                Dim net, spray, injection As Integer
+
+                net = CInt(InputBox("Of those infected with Malaria in week " + CStr(i) + " how many people were using a mosquito net?"))
+                spray = CInt(InputBox("Of those infected with Malaria in week " + CStr(i) + " how many people were using a mosquito repellent spray?"))
+                injection = CInt(InputBox("Of those infected with Malaria in week " + CStr(i) + " how many people had the Malaria injection?"))
+
+                tempMalaria.Net += net
+                tempMalaria.Spray += spray
+                tempMalaria.Injection += injection
+
+                Diseases(1) = tempMalaria
+
+            End If
+
+            'TB
+            tempTB = TryCast(Diseases(2), TB)
+            If (tempTB) Then
+
+                Dim active As Integer
+
+                active = CInt(InputBox("Of those infected with TB in week " + CStr(i) + " how many people have active TB?"))
+
+                tempTB.areActive += active
+
+                Diseases(2) = tempTB
+
+            End If
+
+        Next
+
+        'Calc Worst
+        Dim worstMortality As Double = 0
+        Dim currentMortality As Double
+        Dim worstIndex As Integer
+
+        For i As Integer = 0 To 2
+
+            currentMortality = Diseases(i).CalcMortalityRate()
+
+            If currentMortality > worstMortality Then
+
+                worstIndex = i
+                worstMortality = currentMortality
+
+            End If
+
+        Next
+
+        'Display Worst
+        If worstIndex = 0 Then
+            txtWorst.Text = "HIV"
+        ElseIf worstIndex = 1 Then
+            txtWorst.Text = "Malaria"
+        ElseIf worstIndex = 2 Then
+            txtWorst.Text = "TB"
+        End If
+
+        'Display Data
+        txtDisplay.Text = "HIV:" & NL _
+            & Diseases(0).Display() & NL & NL _
+            & "Malaria:" & NL _
+            & Diseases(1).Display() & NL & NL _
+            & "TB:" & NL _
+            & Diseases(2).Display()
+
+        'Save To File
+        Dim FS As FileStream
+        Dim BF As BinaryFormatter
+
+        FS = New FileStream("Data.txt", FileMode.Create, FileAccess.Write)
+        BF = New BinaryFormatter()
+
+        BF.Serialize(FS, Diseases)
+
+        FS.Close()
+
+    End Sub
+
 End Class
