36b6de1c3ae7858eb58da9aad93176e08530bc94
MICHAEL CAMERON RAMOS KARNER <217028123@student.uj.ac.za>
2020-10-11 16:05:23 +0200

    test

---
+++ GroupProject_Session1_Group6/My Project/Class1.vb
@@ -0,0 +1,18 @@
+﻿' *****************************************************************
+' Team Number: 6
+' Team Member 1 Details: Gerber, Josh 220052458
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' Class name: Disease
+' *****************************************************************
+
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class Class1
+
+End Class
